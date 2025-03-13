26ef093494034bd267aa9fa3f0a8159a3c239f0d
abolfazlsli <140882091+abolfazlsli@users.noreply.github.com>
2025-03-13 14:13:19 +0330

    added form fields

--- src/screens/addbrands.jsx
+++ src/screens/addbrands.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import { createBrand } from "../network/api";
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
+import { Dropdown } from 'primereact/dropdown';
 import React from "react";
 
 const AddBrands = () => {
     const [feildsui , setFeildsui] = useState([]);
+    const [datatosend , setDatatosend] = useState()
     const [pagedata , setPageData] = useState({
         brandname : "" ,
         feilds : []
@@ -15,18 +17,25 @@ const AddBrands = () => {
     }
     const addFeildUi = () => {
         setFeildsui([...feildsui , (
-            <div className="w-[100%] h-[100px] flex flex-col justify-center items-center rounded-lg p-[10px]">
-                <div className="flex justify-start items-center w-[100%] p-[10px]">
-                    <h1 className="text-nowrap mr-[10px]">
-                        نوع فیلد
-                    </h1>
-                    {/* <Select options={["text", "number", "date", "boolean", "select", "multi-select"]} value={pagedata.feilds[0].type} onChange={(e) => setPageData({...pagedata, feilds : [...pagedata.feilds, {type : e.value}]})} /> */}
+            <div className="w-[100%] flex flex-col justify-center items-center rounded-lg p-[10px]">
+                <div className="w-[100%]">
+                    <InputText type="text" className="w-[100%]" placeholder="نام فیلد"  />
                 </div>
-                <div className="flex justify-start items-center w-[100%] p-[10px]">
-                    <InputText type="text" placeholder="نام فیلد"  />
-                </div>
-                <div className="flex justify-end items-center w-[100%] p-[10px]">
-                    <Button type="primary" onClick={saveFeilds} label="افزودن" />
+                <div className="w-[100%] mt-[10px] mb-[10px]">
+                    <Dropdown className="w-[100%]" placeholder="نوع فیلد" options={[
+                        {
+                            label : "متن",
+                            value : "text"
+                        },
+                        {
+                            label : "عدد",
+                            value : "number"
+                        },
+                        {
+                            label : "رنگ",
+                            value : "color"
+                        },
+                    ]} />
                 </div>
             </div>
         )]);
@@ -42,28 +51,29 @@ const AddBrands = () => {
         })
     }
     return (
-        <div>
-            <div className="w-[100%] h-[100px] flex justify-end items-center rounded-lg p-[10px]">
-                <h1 className="text-2xl font-bold text-right">
-                    افزودن برند
-                </h1>
-            </div>
-            <div className="w-[100%] overflow-y-scroll h-[400px] flex flex-col justify-start items-center rounded-lg p-[10px]">
-                <div className="flex justify-start items-center w-[100%] p-[10px]">
-                    <h1 className="text-nowrap mr-[10px]">
-                        نام برند
+        <div className="flex justify-center items-center" >
+            <div className="w-[80%] flex flex-col justify-start items-center rounded-lg p-[10px] shadow-lg">
+                <div className="w-[100%] h-[50px] flex justify-end items-center rounded-lg">
+                    <h1 className="text-xl font-bold text-right">
+                        افزودن برند
                     </h1>
-                    <InputText type="text" placeholder="نام برند" value={pagedata.brandname} onChange={(e) => setPageData({...pagedata, brandname : e.target.value})} />
                 </div>
-
-                <div className="flex flex-col justify-start items-center w-[100%] p-[10px]">
-                    {feildsui}
-                    <Button type="primary" onClick={addFeildUi}>
-                        افزودن فیلد
-                    </Button>
+                <div className="flex justify-start items-center w-[100%] pb-[10px]">
+                        <h1 className="text-nowrap mr-[10px]">
+                            نام برند
+                        </h1>
+                        <InputText type="text" className="w-[100%]" placeholder="نام برند" value={pagedata.brandname} onChange={(e) => setPageData({...pagedata, brandname : e.target.value})} />
+                    </div>
+                <div className="w-[100%] overflow-y-scroll h-[400px] flex flex-col justify-start items-center rounded-lg p-[10px]">
+                    <div className="flex flex-col justify-start items-center w-[100%] p-[10px]">
+                        {feildsui}
+                        <Button type="primary" onClick={addFeildUi}>
+                            افزودن فیلد
+                        </Button>
+                    </div>
                 </div>
                 <div className="flex justify-end items-center w-[100%] p-[10px]">
-                    <Button type="primary" onClick={sendHandler} className="w-[100%]" label="ثبت" />
+                        <Button type="primary" onClick={sendHandler} className="w-[100%]" label="ثبت" />
                 </div>
             </div>
         </div>
